e671cd10aa243316d9dc332dcad1405521102b02
will i <will.ingarfield@gmail.com>
2017-04-05 16:13:28 -0500

    Create server_main.py

---
+++ server_main.py
@@ -0,0 +1,33 @@
+import socket
+from threading import Thread
+
+
+class ClientHandler(Thread):
+    def __init__(self, cs, address):
+        self.cs = cs
+        self.address = address
+        self.cs.setblocking(0)
+
+    def run(self):
+        while 1:
+            try:
+                data = self.cs.recv(512)
+                print str(address(0)) + data
+            except socket.error:
+                '''nothing'''
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+PORT = 1234
+s.bind(("0.0.0.0", PORT))
+s.listen(5)
+print "server started on " + socket.gethostbyname(socket.gethostname()) + ", " + str(PORT)
+
+while 1:
+    print "looking for client"
+    client_socket, address = s.accept()
+    client_socket.sendall("hello, world!")
+    print "client found"
+    ch = ClientHandler(client_socket, address)
+    ch.run()
