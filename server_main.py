dc639f9379b3d4a05a90092cfcd32bf4dd9cbc1a
will-i <will.ingarfield@gmail.com>
2017-04-08 13:10:39 -0500

    Add proper support for multiple clients

--- server_main.py
+++ server_main.py
@@ -1,7 +1,9 @@
 import socket
 from threading import Thread
+import json
 
 
+# handler to receive data from client class
 class ClientHandler(Thread):
     def __init__(self, cs, address):
         Thread.__init__(self)
@@ -9,14 +11,28 @@ class ClientHandler(Thread):
         self.address = address
         self.cs.setblocking(0)
 
+    def recvall(self, sock, buff):
+        data = ""
+        while 1:
+            part = sock.recv(buff)
+            data += part
+            print part
+            if len(part) < buff:
+                break  # exit loop on last part of message
+        return data
+
     def run(self):
         while 1:
             try:
-                data = self.cs.recv(512)
-                print address[0] + " > " + data
+                msg = self.recvall(self.cs, 1024)
+                print address[0] + " > " + msg
             except socket.error:
                 '''nothing'''
 
+
+
+
+clients = []
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -31,6 +47,10 @@ while 1:
     client_socket.sendall("hello, world!")
     print "client found"
     ch = ClientHandler(client_socket, address)
-    print "client connected"
     ch.start()
+
+    clients.append(ch)
+
+    print "client connected"
     print "client thread started"
+    print clients
