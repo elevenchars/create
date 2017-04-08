bf57725e7eda062720f485de7688a30d4205af37
will-i <will.ingarfield@gmail.com>
2017-04-08 13:10:03 -0500

    deprecating, moving verification and functionality to client class. Will make usable for sending JSON to server by hand

--- client_test.py
+++ client_test.py
@@ -1,4 +1,5 @@
 import socket
+import json
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 SERVER = raw_input("Please type in the server's address: ")
@@ -7,6 +8,9 @@ PORT = int(raw_input("Please type in the server's port: "))
 print "connecting to server"
 s.connect((SERVER, PORT))
 print "connected!"
-
+resp = ""
+#while(json.load(resp)["verified"] != "true"):
+#    name = raw_input("sorry, that name is invalid")
+name = raw_input("what is your name?")
 while 1:
     s.sendall(raw_input("input: "))
