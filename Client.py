200b2eb9ee97b765ba9e4e517a61ecb0f51851fc
will-i <will.ingarfield@gmail.com>
2017-04-11 08:47:18 -0500

    refactor variable names to match PEP8

--- Client.py
+++ Client.py
@@ -2,20 +2,19 @@ from threading import Thread
 import socket
 import json
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+__s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
 def connect(ip, port):
-    s.connect((ip, port))
-    s.sendall(json.dump({"type" : "status",
+    __s.connect((ip, port))
+    __s.sendall(json.dump({"type" : "status",
                          "body" : "newconn"}))
-    resp = json.load(recvall(s))
+    resp = json.load(recvall(__s))
     if("type" in resp and "body" in resp):
         if(resp["type"] == "status" and resp["body"] == "confirm"):
             return True
     else:
         return False
-
 
 def recvall(sock, buff):
     data = ""
@@ -27,14 +26,19 @@ def recvall(sock, buff):
             break  # exit loop on last part of message
     return data
 
-class server_listener(Thread):  # class to receive messages from server, must connect first
+
+def send(sock, content):
+    sock.sendall(content)
+
+
+class ServerListener(Thread):  # class to receive messages from server, must connect first
     def __init__(self, callback):
         Thread.__init__(self)
         self.callback = callback
 
     def run(self):
         while 1:
-            content = json.load(recvall(s, 1024))
+            content = json.load(recvall(__s, 1024))
             if(type in content):
                 if(content["type"] == "message"):
                     self.callback(content)
