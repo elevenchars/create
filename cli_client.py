1ac4c58059970ac48ebeda21aeac10b491a380fb
will-i <will.ingarfield@gmail.com>
2017-04-11 09:57:14 -0500

    worky worky on client

--- cli_client.py
+++ cli_client.py
@@ -1,2 +1,20 @@
 import Client
+import json
 
+def response_printer(resp):
+    print json.dump(resp)
+
+success = False
+while(not success):
+    ip = raw_input("please enter the server IP")
+    port = int(raw_input("please enter the server port"))
+    success = Client.connect(ip, port)
+
+print "connection successful"
+
+sl = Client.ServerListener(response_printer)
+sl.start()
+
+while 1:
+    Client.send({"type" : "message",
+                 "body" : raw_input("message > ")})
