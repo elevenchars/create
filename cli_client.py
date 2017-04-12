98ca136ad590ea15f60036f5b52ef06480911bed
will-i <will.ingarfield@gmail.com>
2017-04-12 15:07:48 -0500

    update client connection

--- cli_client.py
+++ cli_client.py
@@ -9,7 +9,7 @@ while(not success):
     ip = raw_input("please enter the server IP: ")
     port = int(raw_input("please enter the server port: "))
     name = raw_input("please enter your username: ")
-    success = Client.connect(ip, port)
+    success = Client.connect(ip, port, name)
 
 print "connection successful"
 
