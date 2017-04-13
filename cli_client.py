c56df419d8ebc5074a8d29fa5fc040c13d69974c
will-i <will.ingarfield@gmail.com>
2017-04-13 10:05:15 -0500

    fix client

--- cli_client.py
+++ cli_client.py
@@ -1,4 +1,4 @@
-import Client
+import client
 import json
 
 def response_printer(resp):
@@ -9,13 +9,13 @@ while(not success):
     ip = raw_input("please enter the server IP: ")
     port = int(raw_input("please enter the server port: "))
     name = raw_input("please enter your username: ")
-    success = Client.connect(ip, port, name)
+    success = client.connect(ip, port, name)
 
 print "connection successful"
 
-sl = Client.ServerListener(response_printer)
+sl = client.ServerListener(response_printer)
 sl.start()
 
 while 1:
-    Client.send({"type" : "message",
+    client.send({"type" : "message",
                  "body" : raw_input("message > ")})
