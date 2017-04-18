01aa24f4a83d65c304841b3277c49a052011a847
will-i <will.ingarfield@gmail.com>
2017-04-18 09:53:57 -0500

    add text frame to MainApplication

--- client_gui.py
+++ client_gui.py
@@ -1,15 +1,29 @@
 import Tkinter as tk
+import tkMessageBox
+import ScrolledText
 import client
 import re
 
 class MainApplication(tk.Frame):
-    def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
+        self.parent.title("chat")
+        self.parent.iconbitmap("gray75")
+
         self.grid()
+        self.quitButton = tk.Button(self, text="QUIT", command=self.quit)
+        self.editArea = ScrolledText.ScrolledText(
+            master=self,
+            wrap=tk.WORD,
+            width=20,
+            height=10
+        )
         self.create_widgets()
 
     def create_widgets(self):
-        self.quitButton = tk.Button(self, text="QUIT", command=self.quit)
+        self.editArea.grid(sticky=tk.W+tk.E)
+        self.editArea.config(state=tk.DISABLED)
         self.quitButton.grid(ipadx=500)
 
 
@@ -17,6 +31,10 @@ class ServerDialog(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
         self.grid()
+        self.parent = parent
+        self.parent.resizable(False, False)
+        self.parent.title("connect")
+        self.parent.iconbitmap("gray12")
 
         self.ip_label = tk.Label(self, text="IP address")
         self.ip_field = tk.Entry(self)
@@ -32,7 +50,7 @@ class ServerDialog(tk.Frame):
         self.ip_label.grid(row=0, sticky=tk.W)
         self.ip_field.grid(row=0, column=1)
 
-        self.port_label.grid(row=1, sticky=tk.W+tk.E)
+        self.port_label.grid(row=1, sticky=tk.W)
         self.port_field.grid(row=1, column=1)
 
         self.name_label.grid(row=2, sticky=tk.W)
@@ -42,18 +60,28 @@ class ServerDialog(tk.Frame):
 
     def connect(self):
         print "attempting connection to " + self.ip_field.get() + ":" + self.port_field.get()
-        if(client.connect(self.ip_field.get(), int(self.port_field.get()),self.name_field.get())):
-            app = MainApplication()
-            app.mainloop()
+        if(self.validate(self.ip_field.get(), self.port_field.get())):
+
+            if(client.connect(self.ip_field.get(), int(self.port_field.get()),self.name_field.get())):
+                self.destroy()
+                app = MainApplication(self.parent)
+                app.mainloop()
+            else:
+                print "connection failed"
+                tkMessageBox.showerror("error!", "connection failed")
         else:
-            print "connection failed"
-            self.destroy()
+            tkMessageBox.showerror("error!", "invalid port or ip address")
 
+    def validate(self, ip, port):
+        ipPattern = re.compile("^(\d{1,3}\.?){4}$")
+        portPattern = re.compile("^(\d{1,5})$")
 
-    def validate(self):
-        ipPattern = re.compile("(\d{1,3}\.?){4}")
+        if(ipPattern.match(ip) and portPattern.match(port)):
+            return True
+        return False
 
-dialog = ServerDialog(None)
+root = tk.Tk()
+dialog = ServerDialog(root)
 dialog.mainloop()
 
 # app = MainApplication()
