c687300a55f60a8acf0c19004db4d3df3b37ccea
will-i <will.ingarfield@gmail.com>
2017-04-25 23:23:38 -0500

    begin work on implementing plugins in server (not functional)

--- server_main.py
+++ server_main.py
@@ -2,6 +2,9 @@ import socket
 from threading import Thread
 import json
 
+import plugins # so i can get __all__
+from plugins import *
+
 
 # handler to receive data from client class
 class ClientHandler(Thread):
@@ -13,6 +16,22 @@ class ClientHandler(Thread):
         self.name = ""
         self.cs.setblocking(1)
         self.stopped = False
+        self.commands = []
+
+    def populate_commands(self):
+        for command_name in plugins.__all__:
+            self.commands.append(getattr(plugins, command_name).Command(self))
+
+    def is_command(self, message):
+        if (message.startsWith("/")):
+            return True
+        return False
+
+    def find_command(self, message):
+        for command in self.commands:
+            if message.split(" ")[:1] == command.get_command():
+                return command
+        return None
 
     def recvall(self, sock, buff):  # make sure all data is received
         data = ""
@@ -25,6 +44,9 @@ class ClientHandler(Thread):
             if len(part) < buff:
                 break  # exit loop on last part of message
         return data
+
+    def send_to_all(self, msg):
+        send_to_clients(msg)
 
     def run(self):  # main loop
         while not self.stopped:
@@ -45,8 +67,19 @@ class ClientHandler(Thread):
                 else:  # send message once confirmed
                     print self.name + " > " + msg["body"]
                     msg["name"] = self.name
-                    send_to_clients(msg)
+                    print msg["body"]
+                    if(self.is_command(msg["body"])):
+                        print "command found"
+                        current = self.find_command(msg["body"])
+                        if(current):
+                            current.execute(msg["body"])
+                        else:
+                            self.cs.sendall(json.dumps({"type": "message",
+                                                        "body": "sorry, command not found"}))
+                    else:
+                        send_to_clients(msg)
             except (socket.error, Exception) as e:
+                print self.name + " !! " + str(e)
                 clients.remove(self)
                 self.stop()
         print "STOPPED " + self.name + " THREAD"
@@ -76,6 +109,7 @@ clients = []
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # create server
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
 PORT = 1234
 s.bind(("0.0.0.0", PORT))
 s.listen(5)
