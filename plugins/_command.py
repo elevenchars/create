e28c8a895d740fc24ca6477020af540a5ba746e5
will-i <will.ingarfield@gmail.com>
2017-04-25 23:22:39 -0500

    update parameters

--- plugins/_command.py
+++ plugins/_command.py
@@ -1,7 +1,19 @@
 class Command:
-    def __init__(self, name, command):
+    def __init__(self, client_handler, name, command):
+        self.client_handler = client_handler
         self.name = name
         self.command = command
 
     def get_command(self):
         return "/" + self.name
+
+    def matches(self, string):
+        if string.startsWith(self.get_command()):
+            return True
+        return False
+
+    def execute(self, string):
+        self.command(string.split(" ")[1:])
+
+    def command(self, args):
+        raise NotImplementedError
