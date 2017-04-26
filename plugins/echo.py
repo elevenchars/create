b4fef5680f61e78ae08da403bac9e2f5a9b97657
will-i <will.ingarfield@gmail.com>
2017-04-25 23:22:53 -0500

    add sample command to test

---
+++ plugins/echo.py
@@ -0,0 +1,10 @@
+import _command
+import json
+
+class Command(_command.Command):
+    def __init__(self, client_handler):
+        _command.Command.__init__(self, client_handler, "echo", self.command)
+
+    def command(self, args):
+        self.client_handler.sendall(json.dumps({"type": "message",
+                                                "body": "ECHO : " + str(args)}))
