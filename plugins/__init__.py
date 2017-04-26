9ec586bb85ef67e4a53742850258be5ddc980cbc
will-i <will.ingarfield@gmail.com>
2017-04-25 23:22:26 -0500

    make sure that it removes modules beginning with _

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -2,6 +2,4 @@ from os.path import dirname, basename, isfile
 import glob
 
 modules = glob.glob(dirname(__file__)+"/*.py")
-__all__ = [basename(f)[:-3] for f in modules if isfile(f) and not f.startswith("_")]
-
-print __all__
+__all__ = [basename(f)[:-3] for f in modules if (isfile(f) and not basename(f).startswith("_"))]
